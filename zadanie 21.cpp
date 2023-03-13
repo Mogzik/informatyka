9af72a897068a80444e5c85abdf32336bdbf4326
Mogzik <113260847+Mogzik@users.noreply.github.com>
2023-03-13 21:40:20 +0100

    Create zadanie 21.cpp

---
+++ zadanie 21.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+char again;
+double x, y;
+do {
+cout << "Podaj x: ";
+cin >> x;
+if (x <= -1) {
+y = 0;
+} else if (x > -1 && x < 0) {
+y = -x;
+} else if (x >= 0 && x < 1) {
+y = x;
+} else if (x >= 1) {
+y = 2 * x - 1;
+}
+cout << "f(" << x << ") = " << y << endl;
+cout << "Czy chcesz powtorzyc program? (J/N): ";
+cin >> again;
+} while (again == 'J' || again == 'j');
+
+kotlin
+Copy code
+return 0;
+}
