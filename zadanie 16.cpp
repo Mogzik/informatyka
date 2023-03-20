d6e4f498a594c678b2c7561a54f7c18de5cdb036
Mogzik <113260847+Mogzik@users.noreply.github.com>
2023-03-20 12:44:39 +0100

    Update zadanie 16.cpp

--- zadanie 16.cpp
+++ zadanie 16.cpp
@@ -2,63 +2,26 @@
 
 using namespace std;
 
+
+int nwd(int m, int n)
+{
+
+
+while (n!=m)
+if(n>m)
+n-=m;
+else
+m-=n;
+return n;
+}
+
 int main()
 {
-int m, n, r;
+	int n,m;
 cout << "Podaj pierwsza liczbe n: ";
 cin >> n;
 cout << "Podaj druga liczbe mniejsza lub rowna pierwszej m: ";
 cin >> m;
-
-if (m > n)
-{
-    int temp = m;
-    m = n;
-    n = temp;
-}
-
-while (m > 0)
-{
-    r = n % m;
-    n = m;
-    m = r;
-}
-
-cout << "NWD to: " <<r<< endl;
-
-return 0;
-}
-
-
-
-
-#include <iostream>
-
-using namespace std;
-
-int main()
-{
-int m, n, nwd;
-cout << "Podaj pierwsza liczbe n: ";
-cin >> n;
-cout << "Podaj druga liczbe mniejsza lub rowna pierwszej m: ";
-cin >> m;
-cout<<"nwd("<<n<<","<<m<<") = "<<nwd<<(n,m);
-cin>> nwd;
-
-if (m > n)
-{
-    int temp = m;
-    m = n;
-    n = temp;
-}
-
-while (m > 0)
-{
-    nwd = n % m;
-    n = m;
-    m = nwd;
-}
-
+cout<<"nwd("<<m<<","<<n<<") = "<<nwd(n,m);
 return 0;
 }
