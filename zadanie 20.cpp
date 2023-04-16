771adeb76f0e834ed25c1b45969c356cb34624b5
Mogzik <113260847+Mogzik@users.noreply.github.com>
2023-04-16 15:18:43 +0200

    Update zadanie 20.cpp

--- zadanie 20.cpp
+++ zadanie 20.cpp
@@ -2,14 +2,19 @@
 using namespace std;
 
 int main() {
-    float bok, pole;
-    cout << "Podaj bok kwadratu: ";
-    cin >> bok;
-    if (bok > 0) {
-        pole = bok * bok;
-        cout << "Pole kwadratu wynosi: " << pole << endl;
-    } else {
-        cout << "Bok musi byc wiekszy od zera" << endl;
-    }
+    char repeat;
+    do {
+        float side, area;
+        cout << "Podaj bok kwadratu w centymetrach: ";
+        cin >> side;
+        if (side > 0) {
+            area = side * side;
+            cout << "Pole kwadratu = " << area << " cm^2" << endl;
+        } else {
+            cout << "Bok musi byc wiekszy od zera" << endl;
+        }
+        cout << "Jesli powtorzyc program wcisnij t(tak),\nw przeciwnym wypadku wcisnij dowolny klawisz literowy:\n";
+        cin >> repeat;
+    } while (repeat == 't' || repeat == 'T');
     return 0;
 }
