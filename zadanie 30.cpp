34cb6f9d404d5160da5963ac2952a8fb285b9701
Mogzik <113260847+Mogzik@users.noreply.github.com>
2023-10-19 08:21:46 +0200

    Create zadanie 30.cpp

---
+++ zadanie 30.cpp
@@ -0,0 +1,15 @@
+#include <cstdlib>
+#include <iostream>
+#include <stdio.h>
+#include <conio.h>
+using namespace std;
+int main(int argc, char *argv[])
+{
+ for ( int i=1;i<=20;i++)
+{ 
+ cout<<"*"<<endl;
+}
+ cout<<endl;
+ system("PAUSE");
+ return EXIT_SUCCESS;
+}
